d30bfe8bdadb85319eaa5ec019c2e8d742422589
Eloi Charra <eloi.charra@gmail.com>
2024-02-05 14:25:41 +0100

    change project structure

---
+++ src/test/java/polytech/projets10/g1/_1tbonnespratiquesgreenit/ApplicationTests.java
@@ -0,0 +1,13 @@
+package polytech.projets10.g1._1tbonnespratiquesgreenit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
