44697fbc1355dbb1eaeac9290299df8e66dc5e15
Eloi Charra <eloi.charra@gmail.com>
2024-02-16 14:22:49 +0100

    change entities api endpoint

--- src/main/java/polytech/projets10/g1/_1tbonnespratiquesgreenit/controllers/OrganisationController.java
+++ src/main/java/polytech/projets10/g1/_1tbonnespratiquesgreenit/controllers/OrganisationController.java
@@ -1,15 +1,13 @@
 package polytech.projets10.g1._1tbonnespratiquesgreenit.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import polytech.projets10.g1._1tbonnespratiquesgreenit.entities.Organisation;
 import polytech.projets10.g1._1tbonnespratiquesgreenit.repositories.OrganisationRepository;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api")
 public class OrganisationController {
 
     private final OrganisationRepository organisationRepository;
