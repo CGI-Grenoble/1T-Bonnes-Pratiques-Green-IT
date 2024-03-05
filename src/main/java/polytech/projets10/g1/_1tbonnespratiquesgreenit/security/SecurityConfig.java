053f38953ae3ecebdb36bb839fd596163f087b84
Eloi Charra <eloi.charra@gmail.com>
2024-03-05 09:42:45 +0100

    modify spring security config to access swagger ui

--- src/main/java/polytech/projets10/g1/_1tbonnespratiquesgreenit/security/SecurityConfig.java
+++ src/main/java/polytech/projets10/g1/_1tbonnespratiquesgreenit/security/SecurityConfig.java
@@ -27,7 +27,8 @@ class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.cors(Customizer.withDefaults())
-            .authorizeHttpRequests(authorize -> authorize
+            .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**").permitAll()
                         .anyRequest().authenticated()
             )
             .oauth2ResourceServer((oauth2) -> oauth2.jwt(
